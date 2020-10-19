6dba9b13deedd56e6b0f5bb798126b4f7a58af4c
JavierMedina11 <javierinformatica1311@gmail.com>
2020-10-20 01:16:43 +0200

    Backend 3.0

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+    origin: "*"
 };
 
 app.use(cors(corsOptions));
@@ -16,17 +16,22 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
-});
+app.use(express.static('public'));
+
+const db = require("./app/models");
+
+// explotation time.
+db.sequelize.sync();
+
+// development time only. Drop and re-sync db.
+// db.sequelize.sync({ force: true }).then(() => {
+//     console.log("Drop and re-sync db.");
+// });
+
+require("./app/routes/bicycle.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+    console.log(`Server is running on port ${PORT}.`);
 });
-
-// app.use(express.static(__dirname + 'public'));
-const db = require("./app/models");
-db.sequelize.sync();
